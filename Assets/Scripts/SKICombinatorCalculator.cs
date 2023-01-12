2e468280de4e76e0bf29e8ec51665b09d4c1dee2
むずでょ <muzudho1@gmail.com>
2023-01-12 21:11:03 +0900

    Next Combinator

--- Assets/Scripts/SKICombinatorCalculator.cs
+++ Assets/Scripts/SKICombinatorCalculator.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 
 public class SKICombinatorCalculator
@@ -11,10 +12,80 @@ public class SKICombinatorCalculator
     /// <returns></returns>
     public static string Run(string expression)
     {
-        // 先頭の１文字を取得
-        var (isOk, nextExpression) = SolveFirst(expression.TrimStart());
+        // 計算過程
+        StringBuilder calculationProcess = new StringBuilder();
+        calculationProcess.AppendLine(expression);
 
-        return nextExpression;
+        bool isOk = true;
+
+        while (isOk)
+        {
+            expression = expression.TrimStart();
+
+            // 先頭の１文字を取得
+            var (isOk2, nextCombinator) = NextCombinator(expression);
+            if (!isOk2)
+            {
+                break;
+            }
+            Debug.Log($"Next combinator:{nextCombinator}");
+
+            (isOk, expression) = SolveFirst(expression.TrimStart());
+            calculationProcess.AppendLine(expression);
+        }
+
+        return calculationProcess.ToString();
+    }
+
+    private static (bool, char) NextCombinator(string expression)
+    {
+        while (0 < expression.Length)
+        {
+            char first = expression[0];
+
+            switch (first)
+            {
+                case 'S':
+                case 'K':
+                case 'I':
+                    return (true, first);
+
+                case '(':
+                    // TODO 対応する `)` を消去する
+                    var nested = 1;
+
+                    for (int i = 1; i < expression.Length; i++)
+                    {
+                        var ch = expression[i];
+
+                        switch (ch)
+                        {
+                            case '(':
+                                nested++;
+                                break;
+                            case ')':
+                                nested--;
+                                if (nested == 0)
+                                {
+                                    // 先頭の `(` と、対応する `)` を消去した数式
+                                    expression = $"{expression.Substring(1, i)}{expression.Substring(i + 1)}";
+                                    goto after_loop;
+                                }
+                                break;
+
+                        }
+                    }
+                after_loop:
+                    break;
+
+                // 計算不能
+                default:
+                    return (false, ' ');
+            }
+        }
+
+        // 空文字列を指定時
+        return (false, ' ');
     }
 
     private static (bool, string) SolveFirst(string expression)
