f423bd5a72b6d70cf7c7e9b644d0e64670ba4600
むずでょ <muzudho1@gmail.com>
2023-01-13 23:42:31 +0900

    テキスト型のパーサー

--- Assets/Scripts/SKICombinatorCalculator.cs
+++ Assets/Scripts/SKICombinatorCalculator.cs
@@ -1,9 +1,5 @@
-using Assets.Scripts.SKICombinatorCalculus;
+using Assets.Scripts.SKICombinatorCalculus.Text;
 using Assets.Scripts.SKICombinatorCalculus.Tree;
-using System.Collections;
-using System.Collections.Generic;
-using System.Text;
-using UnityEngine;
 
 internal class SKICombinatorCalculator
 {
@@ -17,63 +13,8 @@ internal class SKICombinatorCalculator
     /// <returns></returns>
     public static string Run(WorkingTree workingTree)
     {
-        // 計算過程
-        StringBuilder calculationProcess = new StringBuilder();
-
+        // テキスト型のパーサー
         string rightText = workingTree.ToString();
-        calculationProcess.AppendLine(rightText);
-
-        string leftText;
-
-        int tired = 0; // 疲れ
-        bool isOk = true;
-
-        while (isOk)
-        {
-            if (tired == 100)
-            {
-                calculationProcess.AppendLine($@"
-Very tired...");
-                break;
-            }
-            tired++;
-
-            // 次に処理するコンビネーター。先頭とは限らない
-            char nextCombinator;
-            (isOk, leftText, nextCombinator, rightText) = RightOfCursor.NextCombinator(calculationProcess, rightText);
-            if (!isOk)
-            {
-                Debug.Log($"[Run] Not found nextCombinator");
-                break;
-            }
-            Debug.Log($"[Run 1] leftText:{leftText} nextCombinator:{nextCombinator} rightText:{rightText}");
-
-            switch (nextCombinator)
-            {
-                case 'S':
-                    (isOk, rightText) = SCombinator.Solve(calculationProcess, rightText);
-                    break;
-                case 'K':
-                    (isOk, rightText) = KCombinator.Solve(calculationProcess, rightText);
-                    break;
-                case 'I':
-                    (isOk, rightText) = ICombinator.Solve(calculationProcess, rightText);
-                    break;
-            }
-
-            if (!isOk)
-            {
-                Debug.Log($"[Run] Failed");
-                break;
-            }
-
-            // 右テキストにまとめる
-            Debug.Log($"[Run 2] leftText:{leftText} rightText:{rightText}");
-            rightText = $"{leftText}{rightText}";
-            leftText = "";
-            calculationProcess.AppendLine(rightText);
-        }
-
-        return calculationProcess.ToString();
+        return TextTypeParser.Parse(rightText);
     }
 }
--- Assets/Scripts/SKICombinatorCalculus/Tree/ILeafNode.cs
+++ Assets/Scripts/SKICombinatorCalculus/Tree/ILeafNode.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Assets.Scripts.SKICombinatorCalculus.Tree
+﻿namespace Assets.Scripts.SKICombinatorCalculus.Tree
 {
     /// <summary>
     /// 葉ノード
     /// 
     /// - 子を持たない
     /// </summary>
-    internal class ILeafNode : INode
+    internal interface ILeafNode : INode
     {
     }
 }
